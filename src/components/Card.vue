23bceaed44bcacd59ecb7bc15584674460d9c072
Chase Terry <chase.terry87@gmail.com>
2021-06-17 11:18:40 -0500

    Fixed issue with config and card link

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link :to="`/meal/${this.id}`">
+  <router-link :to="`/meal/${id}`">
     <div class="card">
       <div v-if="image" class="card-image">
         <figure class="image is-4by3">
